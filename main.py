1adb33659e76c6df75258d21cdbe93dbf20c344c
aucetin <tuvmhstda@gmail.com>
2024-10-17 09:57:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,10 @@ import fitz  # pymupdf
 
 # Load CSS file
 def local_css(file_name):
-    with open(r'./styles/homepgStyle.css') as f:
-        css = f.read()) as f:
-    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-local_css("homepgStyle.css")  # Load custom CSS
+local_css("./styles/homepgStyle.css")  # Load custom CSS
 
 # Page configuration
 st.set_page_config(
